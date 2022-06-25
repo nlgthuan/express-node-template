f677a4847fe8b4ea0357ee048d92a51df73fd007
thuan.ngo <thuan.ngo@eastagile.com>
2022-06-25 16:23:03 +0700

    [f] - Add isAuthenticated middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,23 @@
+import { StatusCodes } from 'http-status-codes';
+import jwt from 'jsonwebtoken';
+
+import config from 'config';
+import { User } from 'models';
+
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+      throw new Error();
+    }
+
+    const decoded = jwt.decode(token, config.jwtKey);
+    const userId = decoded.id;
+    req.user = await User.findByPk(userId);
+    return next();
+  } catch (err) {
+    return res.status(StatusCodes.UNAUTHORIZED).send('Invalid Credentials!');
+  }
+};
+
+export default isAuthenticated;
